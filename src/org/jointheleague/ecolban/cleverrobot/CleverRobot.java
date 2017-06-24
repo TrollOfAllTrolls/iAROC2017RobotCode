a9f118e271e8f47e14debe939a60669bbbd11de5
eshaank <eshaank@gmail.com>
2017-06-24 08:42:33 -0700

    Updated Code

--- src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
+++ src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
@@ -16,17 +16,19 @@ public class CleverRobot extends IRobotAdapter {
 	Sonar sonar = new Sonar();
 	private boolean tailLight;
 	Camera cam;
-	int sideDist;
-	int sonarDist = 150;
+	int distance;
+	int[] lights;
 	int[] pix;
 	int camRuns = 0;
 	Random rand = new Random();
 	int randomNumber;
-	double redPercent;
+	double redPercent = 0;
 	boolean isInField = false;
-	// runType 0 = Maze || runType 1 = DragRace || runType 2 = GoldRush ||
-	// runType 3 = BackupMaze(NoCamera)
-	int runType = 2;
+	// runType 0 = Maze || runType 1 = DragRace || runType 2 = GoldRush ||
+	// runType 3 = LeftMaze
+	int runType = 0;
+	int useFrontLight = 1;
+	int useCam = 1;
 
 	public CleverRobot(IRobotInterface iRobot) {
 		super(iRobot);
@@ -44,7 +46,7 @@ public class CleverRobot extends IRobotAdapter {
 	}
 
 	private void setup() throws Exception {
-		if (runType == 0) {
+		if (runType == 0 && useCam == 1 || runType == 3 && useCam == 1) {
 			// Maze Code
 			cam = new Camera(150, 50);
 			cam.enableBurst();
@@ -71,21 +73,26 @@ public class CleverRobot extends IRobotAdapter {
 
 	private boolean loop() throws Exception {
 		readSensors(100);
-		sideDist = getWallSignal();
+		distance = getWallSignal();
+		lights = getLightBumps();
+		System.out.println(getInfraredByte());
 		if (runType == 0) {
 			// Maze Code
-			if (isBumpRight() || isBumpLeft()) {
+			if (lights[2] + lights[3] > 2 && getInfraredByte() == 0 && useFrontLight == 1) {
+				driveDirect(-500, 500);
+				Thread.sleep(325);
+			} else if (isBumpRight() || isBumpLeft()) {
 				driveDirect(-500, -500);
 				Thread.sleep(100);
 				driveDirect(-500, 500);
 				Thread.sleep(325);
-			} else if (redPercent > 8) {
+			} else if (redPercent > 8 && useCam == 1) {
 				System.out.println("Found Red");
 				driveDirect(-500, 500);
 				Thread.sleep(750);
 				driveDirect(400, 400);
 				Thread.sleep(1000);
-			} else if (sideDist > 6) {
+			} else if (distance > 6) {
 				driveDirect(200, 500);
 			} else {
 				driveDirect(500, 110);
@@ -97,44 +104,62 @@ public class CleverRobot extends IRobotAdapter {
 				Thread.sleep(350);
 				driveDirect(-500, 500);
 				Thread.sleep(400);
-			} else if (sideDist > 2) {
-				driveDirect(320, 500);
+			} else if (lights[2] + lights[3] > 2) {
+				driveDirect(-500, 500);
+				Thread.sleep(400);
+			} else if (distance > 2) {
+				driveDirect(350, 500);
 			} else {
 				driveDirect(500, 450);
 			}
 		} else if (runType == 2) {
 			// GoldRush Code
-			System.out.println("Run");
-			if (true) {
-				randomNumber = rand.nextInt(700) + 400;
-				// if(){
-				//
-				// }
-				if (isBumpRight()) {
-					driveDirect(-500, -500);
-					Thread.sleep(150);
-					driveDirect(-300, 300);
-					Thread.sleep(randomNumber);
-				} else if (isBumpLeft()) {
-					driveDirect(-500, -500);
-					Thread.sleep(150);
-					driveDirect(300, -300);
-					Thread.sleep(randomNumber);
+			randomNumber = rand.nextInt(300) + 200;
+			if (getInfraredByte() != 0 || isInField == false) {
+				if (isBumpLeft() || isBumpRight()) {
+					isInField = true;
+				} else if (getInfraredByte() == 244 || getInfraredByte() == 246) {
+					driveDirect(500, 200);
+				} else if (getInfraredByte() == 248 || getInfraredByte() == 250) {
+					driveDirect(200, 500);
 				} else {
-					driveDirect(500, 500);
+					driveDirect(300, 300);
 				}
+			} else if (isBumpRight()) {
+				driveDirect(-500, -500);
+				Thread.sleep(150);
+				driveDirect(-300, 300);
+				Thread.sleep(randomNumber);
+			} else if (isBumpLeft()) {
+				driveDirect(-500, -500);
+				Thread.sleep(150);
+				driveDirect(300, -300);
+				Thread.sleep(randomNumber);
+			} else {
+				isInField = false;
+				driveDirect(500, 500);
 			}
+
 		} else if (runType == 3) {
-			// Backup Maze Code
-			if (isBumpRight() || isBumpLeft()) {
+			// Left Maze Code
+			if (lights[2] + lights[3] > 2 && getInfraredByte() == 0 && useFrontLight == 1) {
+				driveDirect(500, -500);
+				Thread.sleep(325);
+			} else if (isBumpRight() || isBumpLeft()) {
 				driveDirect(-500, -500);
 				Thread.sleep(100);
-				driveDirect(-500, 500);
+				driveDirect(500, -500);
 				Thread.sleep(325);
-			} else if (sideDist > 6) {
-				driveDirect(200, 500);
+			} else if (redPercent > 8 && useCam == 1) {
+				System.out.println("Found Red");
+				driveDirect(500, -500);
+				Thread.sleep(750);
+				driveDirect(400, 400);
+				Thread.sleep(1000);
+			} else if (lights[0] > 2) {
+				driveDirect(500, -400);
 			} else {
-				driveDirect(500, 110);
+				driveDirect(110, 500);
 			}
 		}
 
